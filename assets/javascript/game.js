166518a58b94a0077191d6db5db3dc20603b75cd
Ornella Hernandez <ohernandez2016@u.northwestern.edu>
2017-02-28 16:42:52 -0600

    final files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,10 +23,16 @@ var animals = [
   var wrongGuesses = []; 
   
   var winCounter = 0; 
-  var lossCounter = 0;
+  var lossCounter = 1;
   var numGuesses = 10;
   
   function startGame() {
+
+    wrongGuesses = []; 
+    numGuesses = 10;
+    correctGuesses = []; 
+    
+
     chosenWord = animals[Math.floor(Math.random() * animals.length)];
     lettersInChosenWord = chosenWord.split("");
     numBlanks = lettersInChosenWord.length;
@@ -59,18 +65,28 @@ var animals = [
         numGuesses --;
         wrongGuesses.push(letter)
     }
-
-
   }
+
 
   function roundComplete(){
-    document.getElementById("word_bank").innerHTML = correctGuesses;
+    document.getElementById("word_bank").innerHTML = correctGuesses.join(" ");
     document.getElementById("guesses_left").innerHTML = numGuesses;
-    document.getElementById("letters_guessed").innerHTML = wrongGuesses;
+    document.getElementById("letters_guessed").innerHTML = wrongGuesses.join(" ");
 
-
-
+    if(lettersInChosenWord.join(" ") === correctGuesses.join(" ")){
+        winCounter++;
+        alert("You win!");
+        document.getElementById("win_counter").innerHTML = winCounter;
+        startGame();
+    }else if(numGuesses === 0){
+        document.getElementById("loss_counter").innerHTML = lossCounter++;
+        document.getElementById("letters_guessed").innerHTML = "";
+        alert("You don't have any more guesses");
+        startGame();
+    }
   }
+
+
   startGame();
   document.onkeyup = function(event) {  
     var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
